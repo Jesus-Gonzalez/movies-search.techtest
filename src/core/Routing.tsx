e77c36576e9a749e54ffa4e668e29c0210b2053b
Jesus Gonzalez <jesus.gonzalez.eu@gmail.com>
2020-05-17 17:53:03 +0100

    feature/error-page: initial version

--- src/core/Routing.tsx
+++ src/core/Routing.tsx
@@ -6,6 +6,7 @@ import LoadingPage from '../app/pages/loading/LoadingPage';
 const HomePage = React.lazy(() => import('../app/pages/home/HomePage'));
 const FilmPage = React.lazy(() => import('../app/pages/film/FilmPage'));
 const HistoryPage = React.lazy(() => import('../app/pages/history/HistoryPage'));
+const ErrorPage = React.lazy(() => import('../app/pages/error/ErrorPage'));
 
 export const Routing = () => {
   return (
@@ -14,6 +15,7 @@ export const Routing = () => {
         fallback={<LoadingPage />}
       >
         <Switch>
+          <Route exact path="/error" component={ErrorPage} />
           <Route exact path="/history" component={HistoryPage} />
           <Route exact path="/search" component={HomePage} />
           <Route exact path="/film/:filmId" component={FilmPage} />
